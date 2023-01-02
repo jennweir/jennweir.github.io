4e8298d3064b7db0f26947266dc0f6009e02f208
Jennifer Weir <jenn@Jennifers-MacBook-Pro.local>
2023-01-02 00:35:58 -0500

    added cool effect to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import GoToTop from '../components/GoToTop';
+import './style.scss';
   
 const Home = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'Center', alignItems: 'Center' }} className="font-link">
-      <h1>Home Page</h1>
-
+      <div class="content">
+        <div class="content__container">
+          <p class="content__container__text">
+            Hello
+          </p>
+          
+          <ul class="content__container__list">
+            <li class="content__container__list__item">world</li>
+            <li class="content__container__list__item">coders</li>
+            <li class="content__container__list__item">creatives</li>
+            <li class="content__container__list__item">everyone!</li>
+          </ul>
+        </div>
+      </div>
       <GoToTop />
     </div>
   );
